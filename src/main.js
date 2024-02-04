27352f34fe3d5e504dc1bce4c6a2c511ce0633c9
Junfa <1170640485fg@gmail.com>
2024-02-04 18:08:38 -0400

    Just 4 fun

--- src/main.js
+++ src/main.js
@@ -1,23 +1,49 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-const server = http.createServer((request, response) => {
-    const data = {
-        id: 1,
-        title: 'Land Down Under',
-        content: 'Can you hear, can you hear the thunder'
-    }
+app.use(express.json());
 
-    const jsonData = JSON.stringify(data);
-
-    response.writeHead(200, {
-        'Content-Type': 'application/json: charset=utf-8'
-    });
-
-    response.write(jsonData);
-
-    response.end();
+app.listen(port, () => {
+    console.log('the service is online!');
 });
 
-server.listen(3000, () => {
-    console.log('The service is online');
+app.get('/', (request, response) => {
+    response.send('hello');
+});
+
+const data = [
+    {
+        "id": 1,
+        "name": "Kobe Bryant",
+        "number": "24"
+    },
+    {
+        "id": 2,
+        "name": "Kyrie Irving",
+        "number": "2"
+    },
+    {
+        "id": 3,
+        "name": "Lebron James",
+        "number": "23"
+    }
+]
+
+app.get('/posts', (request, response) => {
+    response.send(data);
+});
+
+app.post('/posts', (request, response) => {
+    const { content } = request.body;
+
+    response.status(201);
+
+    console.log(request.header["Sing-a-song"]);
+
+    response.set('Message-From-Hell', 'Your man is dead');
+
+    response.send({
+        message: 'Successfully Created: ${content}'
+    })
 });
